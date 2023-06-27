c20b5c47108ed5938b25c8d2b0a8e2054dbbe89f
notkillua07 <112600181+notkillua07@users.noreply.github.com>
2023-06-27 13:50:19 +0700

    v 1.1

--- order.php
+++ order.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if (isset($_SESSION['food'])) {
+    $arr_food = $_SESSION['food'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,34 +13,73 @@ session_start();
     <title>Document</title>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <style type="text/css">
+        p{font-family: Arial, Helvetica, sans-serif;}
+        #container {
+            display: grid;
+            grid-template-columns: auto 250px;
+        }
+
         #food-container {
             display: grid;
             grid-template-columns: auto auto auto auto;
             gap: 10px;
             padding: 10px;
-            width: 750px;
             background: blue;
         }
 
-        #food-container> .card {
-            background-color: rgba(255, 255, 255, 0.8);
+        .card {
+            background-color: red;
             text-align: center;
-            padding: 20px 0;
-            font-size: 12px;
+            height: 300px;
+            padding: 20px 10px;
+            
+            max-width: 300px;
         }
 
         #receipt-container {
             width: 250px;
-            background: red;
+            background: blue;
+        }
+
+        img {
+            aspect-ratio: 1/1;
+            object-fit: cover;
+            max-width: 247px;
+        }
+
+        body {
+            margin: 0px;
+        }
+
+        .text-container {
+            text-align: left;
+            font-size: 24px;
+            padding: 0 20px
         }
     </style>
 </head>
 
 <body>
     <div id="container">
-        <div id="food-containter">
-            <div class="card">1</div>
-
+        <div id="food-container">
+            <?php
+            foreach ($arr_food as $kode => $arrfood) {
+                $url = $arrfood[2];
+                $nama = $arrfood[0];
+                $harga = $arrfood[1];
+                echo "<div class=\"card\" kode=\"$kode\">
+                    <div class=\"img-container\">
+                        <img src=\"$url\">
+                    </div>
+                    <div class=\"text-container\">
+                        <p>
+                        " . $nama . "<br>
+                        Rp. " . $harga . "
+                        </p>
+                    </div>
+                </div>";
+            }
+            ?>
         </div>
         <div id="receipt-container">
 
